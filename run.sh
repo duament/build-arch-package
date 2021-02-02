4e3fa312842027f93add4939ef06d59e9e081f03
duament <30264485+duament@users.noreply.github.com>
2021-02-03 02:11:24 +0800

    Support making a release

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -ex
 
-if [ -z "$INPUT_PKGBUILD" ] || [ -z "$INPUT_OUTDIR" ] || [ -z "$GITHUB_SHA" ]; then
+if [ -z "$INPUT_DIR" ] || [ -z "$TOKEN" ] || [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_API_URL" ] || [ -z "$GITHUB_REPOSITORY" ]; then
     echo 'Missing environment variables'
     exit 1
 fi
 
 # Resolve environment paths
-INPUT_PKGBUILD="$(eval echo $INPUT_PKGBUILD)"
-INPUT_OUTDIR="$(eval echo $INPUT_OUTDIR)"
+INPUT_DIR="$(eval echo $INPUT_DIR)"
+
+# Add custom repo
+cp pacman-duama.conf /etc/pacman.conf
 
 # Prepare the environment
-pacman -Syu --noconfirm --noprogressbar --needed base-devel devtools btrfs-progs dbus sudo
+pacman -Syu --noconfirm --noprogressbar --needed base-devel devtools devtools-duama btrfs-progs dbus sudo jq
 
 dbus-uuidgen --ensure=/etc/machine-id
 
@@ -19,18 +21,52 @@ sed -i "s|MAKEFLAGS=.*|MAKEFLAGS=-j$(nproc)|" /etc/makepkg.conf
 useradd -m user
 cd /home/user
 
-# Copy PKGBUILD and *.install scripts
-cp "$INPUT_PKGBUILD"/* ./ || true
+# Copy PKGBUILD and other files
+cp "$INPUT_DIR"/* ./ || true
 chown user PKGBUILD
 
 # Build the package
-extra-x86_64-build -- -U user $(echo $INPUT_PARAM)
+duama-x86_64-build -- -U user
 
-# Save the artifacts
-mkdir -p "$INPUT_OUTDIR"
-cp $(ls *.pkg.* | grep -v '\.log$') "$INPUT_OUTDIR"/
+rm -f *.log
 
-pkg_path=$(ls -d "$INPUT_OUTDIR"/*)
-pkg_name=$(ls "$INPUT_OUTDIR")
-echo "::set-output name=pkg_path::$pkg_path"
-echo "::set-output name=pkg_name::$pkg_name"
+basename=$(basename "INPUT_DIR")
+first_pkg_filename=$(ls *.pkg.tar.* | head -n 1)
+t=${first_pkg_filename%-*}
+pkgrel=${t##*-}
+t=${t%-*}
+pkgver=${t##*-}
+
+# Create a release
+res=$(curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "Content-type: application/json" \
+    "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases" \
+    -d "{\"tag_name\":\"$basename-$(date +%Y%m%d%H%M)\", \"name\":\"$basename: $pkgver-$pkgrel\"}")
+
+upload_url=$(echo "$res" | jq '.upload_url')
+upload_url=${upload_url%\{*}
+
+for filename in *.pkg.tar.*
+do
+    # Upload release asset
+    res=$(curl \
+        -X POST \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "Content-Type: $(file -b --mime-type $filename)" \
+        --data-binary @$filename \
+	"$upload_url?name=$filename")
+    
+    browser_download_url=$(echo "res" | jq '.browser_download_url')
+    
+    # Trigger repo-add
+    curl \
+        -X POST \
+        -H "Token: $TOKEN" \
+        'https://repo.duama.top/repo-add' \
+        -d "$browser_download_url"
+done
+
