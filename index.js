599686cbd69c9bf6dde176db58e8da23f40ce430
duament <30264485+duament@users.noreply.github.com>
2021-02-03 13:30:56 +0800

    Switch to composite run steps action

--- index.js
+++
@@ -1,4 +0,0 @@
-const script = 'run.sh'
-const script_path = require('path').resolve(__dirname, script)
-
-require('child_process').spawn(script_path, [], { stdio: 'inherit' }).on('exit', code => process.exit(code))
